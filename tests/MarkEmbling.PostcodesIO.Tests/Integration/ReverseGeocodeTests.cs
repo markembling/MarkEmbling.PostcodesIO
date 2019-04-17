2422558c43e0b4c413bf35b06fbb2336dbc38592
Mark Embling <mark@markembling.info>
2019-04-17 20:16:31 +0100

    Implement outcode reverse geocoding and tests

--- tests/MarkEmbling.PostcodesIO.Tests/Integration/ReverseGeocodeTests.cs
+++ tests/MarkEmbling.PostcodesIO.Tests/Integration/ReverseGeocodeTests.cs
@@ -69,5 +69,48 @@ namespace MarkEmbling.PostcodesIO.Tests.Integration
             //TODO probably a better way of writing this without using Assert.IsTrue
             Assert.IsTrue(results.Any(p => p.Postcode == "GU1 1AA"));
         }
+
+
+        [Test]
+        public void OutwardCodeReverseGeocode_returns_populated_response()
+        {
+            var results = _client.OutwardCodeReverseGeocode(51.2430302947367, -0.564888615311003);
+            Assert.True(results.Any());
+        }
+
+        [Test]
+        public async Task OutwardCodeReverseGeocodeAsync_returns_populated_response()
+        {
+            var results = await _client.OutwardCodeReverseGeocodeAsync(51.2430302947367, -0.564888615311003);
+            Assert.True(results.Any());
+        }
+
+        [Test]
+        public void OutwardCodeReverseGeocode_with_limit_returns_limited_results()
+        {
+            var results = _client.OutwardCodeReverseGeocode(51.2430302947367, -0.564888615311003, limit: 2);
+            Assert.AreEqual(2, results.Count());
+        }
+
+        [Test]
+        public async Task OutwardCodeReverseGeocodeAsync_with_limit_returns_limited_results()
+        {
+            var results = await _client.OutwardCodeReverseGeocodeAsync(51.2430302947367, -0.564888615311003, limit: 2);
+            Assert.AreEqual(2, results.Count());
+        }
+
+        [Test]
+        public void OutwardCodeReverseGeocode_with_wide_radius_gives_more_results()
+        {
+            var results = _client.OutwardCodeReverseGeocode(51.2430302947367, -0.564888615311003, radius: 10000);
+            Assert.AreEqual(10, results.Count());
+        }
+
+        [Test]
+        public async Task OutwardCodeReverseGeocodeAsync_with_wide_radius_gives_more_results()
+        {
+            var results = await _client.OutwardCodeReverseGeocodeAsync(51.2430302947367, -0.564888615311003, radius: 10000);
+            Assert.AreEqual(10, results.Count());
+        }
     }
 }
