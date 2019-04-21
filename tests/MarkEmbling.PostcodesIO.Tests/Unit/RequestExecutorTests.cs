c340e035823a7dfb7be321fba08e9b7ce4ae4b67
Mark Embling <mark@markembling.info>
2019-04-21 19:42:39 +0100

    Make sure all the async methods on RequestExecutor are tested

--- tests/MarkEmbling.PostcodesIO.Tests/Unit/RequestExecutorTests.cs
+++ tests/MarkEmbling.PostcodesIO.Tests/Unit/RequestExecutorTests.cs
@@ -86,7 +86,7 @@ namespace MarkEmbling.PostcodesIO.Tests.Unit
         }
 
         [Test]
-        public void Execute_methods_return_empty_collection_instead_of_null()
+        public void ExecuteRequest_returns_empty_collection_instead_of_null()
         {
             var clientMock = new Mock<IRestClient>();
             var responseMock = new Mock<IRestResponse<RawResult<List<object>>>>();
@@ -104,7 +104,7 @@ namespace MarkEmbling.PostcodesIO.Tests.Unit
         }
 
         [Test]
-        public void Execute_methods_return_null_if_its_not_a_collection()
+        public void ExecuteRequest_returns_null_if_its_not_a_collection()
         {
             var clientMock = new Mock<IRestClient>();
             var responseMock = new Mock<IRestResponse<RawResult<object>>>();
@@ -138,6 +138,95 @@ namespace MarkEmbling.PostcodesIO.Tests.Unit
             clientMock.Verify();
         }
 
-        
+        [Test]
+        public async Task ExecuteRequestAsync_returns_the_result_when_there_is_one()
+        {
+            var resultData = new object();
+
+            var clientMock = new Mock<IRestClient>();
+            var responseMock = new Mock<IRestResponse<RawResult<object>>>();
+            var rawResultMock = new Mock<RawResult<object>>();
+
+            clientMock.Setup(x => x.ExecuteTaskAsync<RawResult<object>>(It.IsAny<IRestRequest>()))
+                .Returns(Task.FromResult(responseMock.Object));
+            responseMock.SetupGet(x => x.Data).Returns(rawResultMock.Object);
+            rawResultMock.SetupGet(x => x.Result).Returns(resultData);
+
+            var sut = new RequestExecutor(clientMock.Object);
+            var result = await sut.ExecuteRequestAsync<object>(new RestRequest());
+
+            Assert.AreSame(resultData, result);
+        }
+
+        [Test]
+        public void ExecuteRequestAsync_throws_exception_when_response_is_null()
+        {
+            var resultData = new object();
+
+            var clientMock = new Mock<IRestClient>();
+            var responseMock = new Mock<IRestResponse<RawResult<object>>>();
+
+            clientMock.Setup(x => x.ExecuteTaskAsync<RawResult<object>>(It.IsAny<IRestRequest>()))
+                .Returns(Task.FromResult(responseMock.Object));
+            responseMock.SetupGet(x => x.Data).Returns((RawResult<object>)null);
+
+            var sut = new RequestExecutor(clientMock.Object);
+            Assert.ThrowsAsync<PostcodesIOEmptyResponseException>(
+                async () => await sut.ExecuteRequestAsync<object>(new RestRequest()));
+        }
+
+        [Test]
+        public void ExecuteRequestAsync_throws_when_request_results_in_error()
+        {
+            var expectedInnerException = new Exception();
+
+            var clientMock = new Mock<IRestClient>();
+            var responseMock = new Mock<IRestResponse<RawResult<object>>>();
+
+            clientMock.Setup(x => x.ExecuteTaskAsync<RawResult<object>>(It.IsAny<IRestRequest>()))
+                .Returns(Task.FromResult(responseMock.Object));
+            responseMock.SetupGet(x => x.ErrorException).Returns(expectedInnerException);
+
+            var sut = new RequestExecutor(clientMock.Object);
+            var resultException = Assert.ThrowsAsync<PostcodesIOApiException>(
+                async () => await sut.ExecuteRequestAsync<object>(new RestRequest()));
+            Assert.AreSame(expectedInnerException, resultException.InnerException);
+        }
+
+        [Test]
+        public async Task ExecuteRequestAsync_returns_empty_collection_instead_of_null()
+        {
+            var clientMock = new Mock<IRestClient>();
+            var responseMock = new Mock<IRestResponse<RawResult<List<object>>>>();
+            var rawResultMock = new Mock<RawResult<List<object>>>();
+
+            clientMock.Setup(x => x.ExecuteTaskAsync<RawResult<List<object>>>(It.IsAny<IRestRequest>()))
+                .Returns(Task.FromResult(responseMock.Object));
+            responseMock.SetupGet(x => x.Data).Returns(rawResultMock.Object);
+            rawResultMock.SetupGet(x => x.Result).Returns((List<object>)null);
+
+            var sut = new RequestExecutor(clientMock.Object);
+            var result = await sut.ExecuteRequestAsync<List<object>>(new RestRequest());
+
+            Assert.AreEqual(new List<object>(), result);
+        }
+
+        [Test]
+        public async Task ExecuteRequestAsync_returns_null_if_its_not_a_collection()
+        {
+            var clientMock = new Mock<IRestClient>();
+            var responseMock = new Mock<IRestResponse<RawResult<object>>>();
+            var rawResultMock = new Mock<RawResult<object>>();
+
+            clientMock.Setup(x => x.ExecuteTaskAsync<RawResult<object>>(It.IsAny<IRestRequest>()))
+                .Returns(Task.FromResult(responseMock.Object));
+            responseMock.SetupGet(x => x.Data).Returns(rawResultMock.Object);
+            rawResultMock.SetupGet(x => x.Result).Returns(null);
+
+            var sut = new RequestExecutor(clientMock.Object);
+            var result = await sut.ExecuteRequestAsync<object>(new RestRequest());
+
+            Assert.IsNull(result);
+        }
     }
 }
