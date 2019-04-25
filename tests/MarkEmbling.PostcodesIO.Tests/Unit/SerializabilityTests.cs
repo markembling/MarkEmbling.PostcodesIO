d1b6028c0fb99fd0b612756050fd945dcdfe35d6
Mark Embling <mark@markembling.info>
2019-04-25 20:07:40 +0100

    Test serializability of PlaceResult

--- tests/MarkEmbling.PostcodesIO.Tests/Unit/SerializabilityTests.cs
+++ tests/MarkEmbling.PostcodesIO.Tests/Unit/SerializabilityTests.cs
@@ -9,37 +9,6 @@ namespace MarkEmbling.PostcodesIO.Tests.Unit
     [TestFixture]
     public class SerializabilityTests
     {
-        static byte[] Serialize(object o)
-        {
-            if (o == null)
-            {
-                return null;
-            }
-
-            var binaryFormatter = new BinaryFormatter();
-            using (var memoryStream = new MemoryStream())
-            {
-                binaryFormatter.Serialize(memoryStream, o);
-                byte[] objectDataAsStream = memoryStream.ToArray();
-                return objectDataAsStream;
-            }
-        }
-
-        static T Deserialize<T>(byte[] stream)
-        {
-            if (stream == null)
-            {
-                return default(T);
-            }
-
-            var binaryFormatter = new BinaryFormatter();
-            using (var memoryStream = new MemoryStream(stream))
-            {
-                T result = (T)binaryFormatter.Deserialize(memoryStream);
-                return result;
-            }
-        }
-
         [Test]
         public void PostcodeResult_should_be_serializable()
         {
@@ -128,6 +97,82 @@ namespace MarkEmbling.PostcodesIO.Tests.Unit
             Assert.AreEqual(expected.Latitude, actual.Latitude);
             Assert.AreEqual(expected.MonthTerminated, actual.MonthTerminated);
             Assert.AreEqual(expected.Postcode, actual.Postcode);
+        }
+
+        [Test]
+        public void PlaceResult_should_be_serializable()
+        {
+            var expected = new PlaceResult
+            {
+                Code = "CODE",
+                Country = "Country",
+                CountyUnitary = "CountryUnitary",
+                CountyUnitaryType = "CountryUnitaryType",
+                DistrictBorough = "DistictBorough",
+                DistrictBoroughType = "DistrictBoroughType",
+                Eastings = 10000,
+                Latitude = 53.9999,
+                LocalType = "LocalType",
+                Longitude = -3.9999,
+                MaxEastings = 10000,
+                MaxNorthings = 20000,
+                MinEastings = 10000,
+                MinNorthings = 20000,
+                Name1 = "Name1",
+                Name1Language = "Name1Language",
+                Name2 = "Name2",
+                Name2Language = "Name2Language",
+                Northings = 20000,
+                Region = "Region"
+            };
+            var expectedBytes = Serialize(expected);
+            var actual = Deserialize<PlaceResult>(expectedBytes);
+
+            Assert.AreEqual(expected.Code, actual.Code);
+            Assert.AreEqual(expected.Country, actual.Country);
+            Assert.AreEqual(expected.CountyUnitary, actual.CountyUnitary);
+            Assert.AreEqual(expected.CountyUnitaryType, actual.CountyUnitaryType);
+            Assert.AreEqual(expected.DistrictBorough, actual.DistrictBorough);
+            Assert.AreEqual(expected.DistrictBoroughType, actual.DistrictBoroughType);
+            Assert.AreEqual(expected.Eastings, actual.Eastings);
+            Assert.AreEqual(expected.Latitude, actual.Latitude);
+            Assert.AreEqual(expected.LocalType, actual.LocalType);
+            Assert.AreEqual(expected.Longitude, actual.Longitude);
+            Assert.AreEqual(expected.MaxEastings, actual.MaxEastings);
+            Assert.AreEqual(expected.MaxNorthings, actual.MaxNorthings);
+            Assert.AreEqual(expected.MinEastings, actual.MinEastings);
+            Assert.AreEqual(expected.MinNorthings, actual.MinNorthings);
+            Assert.AreEqual(expected.Name1, actual.Name1);
+            Assert.AreEqual(expected.Name1Language, actual.Name1Language);
+            Assert.AreEqual(expected.Name2, actual.Name2);
+            Assert.AreEqual(expected.Name2Language, actual.Name2Language);
+            Assert.AreEqual(expected.Northings, actual.Northings);
+            Assert.AreEqual(expected.Region, actual.Region);
+        }
+
+        private static byte[] Serialize(object obj)
+        {
+            if (obj == null) return null;
+
+            var binaryFormatter = new BinaryFormatter();
+            using (var memoryStream = new MemoryStream())
+            {
+                binaryFormatter.Serialize(memoryStream, obj);
+                byte[] objectDataAsStream = memoryStream.ToArray();
+                return objectDataAsStream;
+            }
+        }
+
+        private static T Deserialize<T>(byte[] stream)
+        {
+            if (stream == null) return default;
+
+            var binaryFormatter = new BinaryFormatter();
+            using (var memoryStream = new MemoryStream(stream))
+            {
+                T result = (T)binaryFormatter.Deserialize(memoryStream);
+                return result;
+            }
         }
     }
 }
