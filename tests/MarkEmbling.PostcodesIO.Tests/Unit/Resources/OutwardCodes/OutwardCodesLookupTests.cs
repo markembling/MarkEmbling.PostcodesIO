4aed293a2cfb1178c14655c288561bdb00826db2
Mark Embling <mark@markembling.info>
2019-05-01 21:31:53 +0100

    Begin adding unit tests for outcodes

---
+++ tests/MarkEmbling.PostcodesIO.Tests/Unit/Resources/OutwardCodes/OutwardCodesLookupTests.cs
@@ -0,0 +1,60 @@
+﻿using MarkEmbling.PostcodesIO.Internals;
+using MarkEmbling.PostcodesIO.Resources;
+using MarkEmbling.PostcodesIO.Results;
+using Moq;
+using NUnit.Framework;
+using RestSharp;
+using System.Threading.Tasks;
+
+namespace MarkEmbling.PostcodesIO.Tests.Unit.Resources.OutwardCodes
+{
+    [TestFixture]
+    public class OutwardCodesLookupTests
+    {
+        private Mock<IRequestExecutor> _requestExecutorMock;
+        private OutwardCodesResource _outcodes;
+
+        [SetUp]
+        public void Setup()
+        {
+            _requestExecutorMock = new Mock<IRequestExecutor>();
+            _outcodes = new OutwardCodesResource(_requestExecutorMock.Object);
+        }
+
+        [Test]
+        public void Lookup_request_method_is_get()
+        {
+            _outcodes.Lookup("XX1");
+            _requestExecutorMock.Verify(
+                x => x.ExecuteRequest<OutwardCodeResult>(
+                    It.Is<RestRequest>(req => req.Method == Method.GET)));
+        }
+
+        [Test]
+        public async Task LookupAsync_request_method_is_get()
+        {
+            await _outcodes.LookupAsync("XX1");
+            _requestExecutorMock.Verify(
+                x => x.ExecuteRequestAsync<OutwardCodeResult>(
+                    It.Is<RestRequest>(req => req.Method == Method.GET)));
+        }
+
+        [Test]
+        public void Lookup_calls_appropriate_resource_url_with_code()
+        {
+            _outcodes.Lookup("XX1");
+            _requestExecutorMock.Verify(x =>
+                x.ExecuteRequest<OutwardCodeResult>(
+                    It.Is<RestRequest>(req => req.Resource == "outcodes/XX1")));
+        }
+
+        [Test]
+        public async Task LookupAsync_calls_appropriate_resource_url_with_code()
+        {
+            await _outcodes.LookupAsync("XX1");
+            _requestExecutorMock.Verify(x =>
+                x.ExecuteRequestAsync<OutwardCodeResult>(
+                    It.Is<RestRequest>(req => req.Resource == "outcodes/XX1")));
+        }
+    }
+}
---
+++ tests/MarkEmbling.PostcodesIO.Tests/Unit/Resources/OutwardCodes/OutwardCodesReverseGeocodeTests.cs
@@ -0,0 +1,21 @@
+﻿using MarkEmbling.PostcodesIO.Internals;
+using MarkEmbling.PostcodesIO.Resources;
+using Moq;
+using NUnit.Framework;
+
+namespace MarkEmbling.PostcodesIO.Tests.Unit.Resources.OutwardCodes
+{
+    [TestFixture]
+    public class OutwardCodesReverseGeocodeTests
+    {
+        private Mock<IRequestExecutor> _requestExecutorMock;
+        private OutwardCodesResource _outcodes;
+
+        [SetUp]
+        public void Setup()
+        {
+            _requestExecutorMock = new Mock<IRequestExecutor>();
+            _outcodes = new OutwardCodesResource(_requestExecutorMock.Object);
+        }
+    }
+}
