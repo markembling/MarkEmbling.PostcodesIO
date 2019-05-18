cc231db8204cc094940ce02772f45db0fe254147
Mark Embling <mark@markembling.info>
2019-05-18 17:15:51 +0100

    Add missing unit tests for postcode bulk reverse geocoding
    The contents of the request body are now verified.

--- tests/MarkEmbling.PostcodesIO.Tests/Unit/Resources/Postcodes/PostcodesBulkReverseGeocodeTests.cs
+++ tests/MarkEmbling.PostcodesIO.Tests/Unit/Resources/Postcodes/PostcodesBulkReverseGeocodeTests.cs
@@ -58,7 +58,73 @@ namespace MarkEmbling.PostcodesIO.Tests.Unit.Resources.Postcodes
                     It.Is<RestRequest>(req => req.Resource == "postcodes")));
         }
 
-        // TODO: test that body is set appropriately
-        // TODO: test JSON serialisation of ReverseGeocodeQuery (somewhere else)
+        [Test]
+        public void BulkReverseGeocode_adds_json_body_to_request()
+        {
+            _postcodes.BulkReverseGeocode(new[] { new ReverseGeocodeQuery() });
+            _requestExecutorMock.Verify(x =>
+                x.ExecuteRequest<List<BulkQueryResult<ReverseGeocodeQuery, List<PostcodeResult>>>>(
+                    It.Is<RestRequest>(req =>
+                        req.Parameters.Count == 1 &&
+                        req.Parameters[0].Type == ParameterType.RequestBody &&
+                        req.Parameters[0].GetType() == typeof(JsonParameter))));
+        }
+
+        [Test]
+        public async Task BulkReverseGeocodeAsync_adds_json_body_to_request()
+        {
+            await _postcodes.BulkReverseGeocodeAsync(new[] { new ReverseGeocodeQuery() });
+            _requestExecutorMock.Verify(x =>
+                x.ExecuteRequestAsync<List<BulkQueryResult<ReverseGeocodeQuery, List<PostcodeResult>>>>(
+                    It.Is<RestRequest>(req =>
+                        req.Parameters.Count == 1 &&
+                        req.Parameters[0].Type == ParameterType.RequestBody &&
+                        req.Parameters[0].GetType() == typeof(JsonParameter))));
+        }
+
+        [Test]
+        public void BulkReverseLookup_request_body_has_expected_data_shape()
+        {
+            var lookupData = new[] { new ReverseGeocodeQuery() };
+            _requestExecutorMock
+                .Setup(x => x.ExecuteRequest<List<BulkQueryResult<ReverseGeocodeQuery, List<PostcodeResult>>>>(It.IsAny<RestRequest>()))
+                .Callback<RestRequest>(restReq =>
+                {
+                    // Get the request body parameter and check that it has a postcodes property
+                    // which contains the list of postcode strings lookupData.
+                    var body = (JsonParameter)restReq.Parameters[0];
+                    var bodyObj = body.Value;
+                    Assert.AreEqual(
+                        lookupData,
+                        bodyObj.GetType().GetProperty("geolocations").GetValue(bodyObj));
+                });
+
+            _postcodes.BulkReverseGeocode(lookupData);
+
+            _requestExecutorMock.VerifyAll();
+        }
+
+        [Test]
+        public async Task BulkReverseGeocodeAsync_request_body_has_expected_data_shape()
+        {
+            var lookupData = new[] { new ReverseGeocodeQuery() };
+            _requestExecutorMock
+                .Setup(x => x.ExecuteRequestAsync<List<BulkQueryResult<ReverseGeocodeQuery, List<PostcodeResult>>>>(It.IsAny<RestRequest>()))
+                .Returns(Task.FromResult(default(List<BulkQueryResult<ReverseGeocodeQuery, List<PostcodeResult>>>)))  // Required for callback to work for async method
+                .Callback<RestRequest>(restReq =>
+                {
+                    // Get the request body parameter and check that it has a postcodes property
+                    // which contains the list of postcode strings in lookupData.
+                    var body = (JsonParameter)restReq.Parameters[0];
+                    var bodyObj = body.Value;
+                    Assert.AreEqual(
+                        lookupData,
+                        bodyObj.GetType().GetProperty("geolocations").GetValue(bodyObj));
+                });
+
+            await _postcodes.BulkReverseGeocodeAsync(lookupData);
+
+            _requestExecutorMock.VerifyAll();
+        }
     }
 }
